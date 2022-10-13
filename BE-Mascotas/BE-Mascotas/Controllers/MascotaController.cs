6944a3d3b2efcb85bb75da66d796bb4f025003a9
Wilian Ruiz Zapata <wruizza@cesde.net>
2022-10-13 16:38:11 -0500

    Creamos primer metodo get para que me debuelva un json en swagr

--- BE-Mascotas/BE-Mascotas/Controllers/MascotaController.cs
+++ BE-Mascotas/BE-Mascotas/Controllers/MascotaController.cs
@@ -1,11 +1,42 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BE_Mascotas.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace BE_Mascotas.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class MascotaController : ControllerBase
+       
     {
+        private readonly AplicationDbContext _context;
+
+        //creamos el contructor para esta clase
+        public MascotaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //creamos un metodo azincrono para me debuelva un Json en swagger
+
+        [HttpGet]
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {  
+                //me traiga todos los elementos de la tabla mascotas
+                var ListMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(ListMascotas);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+        }
+
     }
 }
