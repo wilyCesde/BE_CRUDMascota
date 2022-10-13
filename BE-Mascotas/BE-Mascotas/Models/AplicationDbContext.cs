dca6a4165c612b64f9ac481e05e5913dc12d9ff5
Wilian Ruiz Zapata <wruizza@cesde.net>
2022-10-13 10:32:26 -0500

    creacion del la carpeta Models , creamos una clase AplicationDbContext y programaos la clase

---
+++ BE-Mascotas/BE-Mascotas/Models/AplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BE_Mascotas.Models
+{  //agregamos dos puntos y la clase DbContext he importamos la clase
+   //https://www.entityframeworktutorial.net/efcore/entity-framework-core.aspx
+   //https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-6.0&tabs=visual-studio
+   //estas url tomamos algunos ejemplos ojo
+    public class AplicationDbContext : DbContext
+    {
+        public  AplicationDbContext(DbContextOptions<AplicationDbContext>options): base(options)
+        {
+         
+        }
+       public DbSet<Mascota>Mascotas { get; set; }
+    }
+}
--- BE-Mascotas/BE-Mascotas/Program.cs
+++ BE-Mascotas/BE-Mascotas/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//agregamos context
+
 
 var app = builder.Build();
 
