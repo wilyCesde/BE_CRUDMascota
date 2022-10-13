b8e841696d367bf79f3415945b3c4cc34a9a58ad
Wilian Ruiz Zapata <wruizza@cesde.net>
2022-10-13 12:23:00 -0500

    agregando paquetes nuget

--- BE-Mascotas/BE-Mascotas/Program.cs
+++ BE-Mascotas/BE-Mascotas/Program.cs
@@ -1,3 +1,7 @@
+using BE_Mascotas.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //agregamos context
-
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+ {
+     options.UseSqlServer;
+ }
+);
 
 var app = builder.Build();
 
