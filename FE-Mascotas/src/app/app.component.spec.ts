65af79ee4a0adba5aa630c6449c1744aa0ab0b53
Wilian Ruiz Zapata <wruizza@cesde.net>
2022-10-18 10:59:49 -0500

    cambia nombre FE_Mascotas

--- FE-Mascotas/src/app/app.component.spec.ts
+++ FE-Mascotas/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FE-CRUDMascotas'`, () => {
+  it(`should have as title 'FE-Mascotas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FE-CRUDMascotas');
+    expect(app.title).toEqual('FE-Mascotas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FE-CRUDMascotas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FE-Mascotas app is running!');
   });
 });
