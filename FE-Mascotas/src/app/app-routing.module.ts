299addeada9e4208c0edd69f33c3f7d207657644
Wilian Ruiz Zapata <wruizza@cesde.net>
2022-10-18 12:00:52 -0500

    se cambio html odo

--- FE-Mascotas/src/app/app-routing.module.ts
+++ FE-Mascotas/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Aca Importamos Los componentes
+// Componentes
 import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
 import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
 import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'listMascotas', pathMatch: 'full' },
-  { path:'listMascotas', component: ListadoMascotaComponent },
-  { path:'agregarMascota', component: AgregarEditarMascotaComponent },
-  { path:'verMascota/:id', component: VerMascotaComponent },
-  { path:'editarMascota/:id', component: AgregarEditarMascotaComponent },
-  { path: '**',  redirectTo: 'listMascotas', pathMatch: 'full' }
+  { path: 'listMascotas', component: ListadoMascotaComponent },
+  { path: 'agregarMascota', component: AgregarEditarMascotaComponent },
+  { path: 'verMascota/:id', component: VerMascotaComponent },
+  { path: 'editarMascota/:id', component: AgregarEditarMascotaComponent },
+  { path: '**', redirectTo: 'listMascotas', pathMatch: 'full' }
 ];
 
 @NgModule({
